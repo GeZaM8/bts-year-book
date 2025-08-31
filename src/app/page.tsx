9af0ab3813cd643db95d21d8f7ecc9add487b3f1
Jean <jeanarby030108@gmail.com>
2025-08-31 19:14:43 +0700

    add default routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import { AArrowDown } from "lucide-react";
+import SiswaCreate from "./admin/siswa/create/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 items-start">
-      <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-        Button
-      </button>
-
-      <div className="p-2 border rounded-lg bg-gray-100 text-gray-700">
-        <AArrowDown className="w-6 h-6" />
-      </div>
-    </div>
+    <SiswaCreate />
   );
 }
